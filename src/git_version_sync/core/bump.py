4d8e99e00026cd2209da869fc9c20855e0966439
Finsa-SC <finsakusumaputra@gmail.com>
2026-09-22 19:02:00 +0700

    feat(bump): add bump logic

---
+++ src/git_version_sync/core/bump.py
@@ -0,0 +1,81 @@
+import subprocess, re
+import tomllib
+from pathlib import Path
+from packaging.version import Version
+from typing import Literal
+
+from .check import parse_highest_verion, get_local_tags, get_config_tag, get_git_path
+
+def bump_git_tag(new_version: Version, message: str|None = None) -> None:
+    command = [
+        'git',
+        'tag',
+        '-a', f'v{new_version}',
+    ]
+    if message and message.strip():
+        command = command + ['-m', message]
+
+    subprocess.run(
+        command,
+        capture_output=True,
+        text=True,
+        check=True
+    )
+
+def bump_config_version(new_version: Version, config_path: Path):
+    content = config_path.read_text(encoding="utf-8")
+
+    pattern = r'^(version\s*=\s*["\']).*?(["\'])'
+    replacement = rf'\g<1>{new_version}\g<2>'
+
+    new_content, count = re.subn(pattern, replacement, content, flags=re.MULTILINE)
+
+    if count == 0:
+        raise RuntimeError(f"Version field not found in {config_path.stem}")
+
+    config_path.write_text(new_content, encoding="utf-8")
+
+def bump_version(new_version: Version, config_path: Path):
+    bump_git_tag(new_version)
+
+def get_new_major(version: Version) -> str:
+    return f"{version.major + 1}.0.0"
+
+def get_new_minor(version: Version) -> str:
+    return f"{version.major}.{version.minor + 1}.0"
+
+def get_new_patch(version: Version):
+    return f"{version.major}.{version.minor}.{version.micro + 1}"
+
+BumpType = Literal["major", "minor", "patch"]
+
+def do_bump(bump_type: BumpType):
+    config_tag = get_config_tag()
+    local_tags = get_local_tags()
+    highest_local_tag = parse_highest_verion(local_tags)
+
+    if config_tag != highest_local_tag:
+        raise RuntimeError(
+            f"Version mismatch detected!\n"
+            f"  Config: v{config_tag}\n"
+            f"  Git   : v{highest_local_tag}\n"
+            f"Please run `git-version-sync sync` first or fix the mismatch."
+        )
+
+    config_path = get_git_path() / "pyproject.toml"
+
+    if not config_path.exists():
+        raise RuntimeError(f"Config file not found: {config_path}")
+
+    new_version = None
+    match bump_type:
+        case "major":
+            new_version = get_new_major(config_tag)
+        case "minor":
+            new_version = get_new_minor(config_tag)
+        case "patch":
+            new_version = get_new_patch(config_tag)
+
+    new_verwion = Version(new_version)
+
+    bump_version(new_version, config_path)
