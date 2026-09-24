0ab4579e0ca5b4e9678424ce241db2761605f177
Finsa-SC <finsakusumaputra@gmail.com>
2026-09-24 16:12:32 +0700

    feat(push): add push code logic

--- src/git_version_sync/core/push.py
+++ src/git_version_sync/core/push.py
@@ -1,6 +1,36 @@
-from .check import get_local_tags, get_remote_tags
+from packaging.version import Version
 
-def do_push(tags: list[str]):
+from .check import get_local_tags, get_remote_tags, parse_highest_verion
+from .git import push_to_remote, fetch_remote_tags
+
+
+def do_push(tags: list[str]|None, push_all: bool=False):
+    fetch_remote_tags()
+
     local_tags = get_local_tags()
-    remote_tags = get_remote_tags()
 
+    tags_to_push: list[str] = []
+
+    if push_all:
+        remote_tags = get_remote_tags()
+        unpush_tags = local_tags - remote_tags
+
+        if unpush_tags:
+            tags_to_push.extend(unpush_tags)
+        else:
+            print("Everything up-to-date.")
+            return
+
+    elif tags is not None:
+        tags_to_push.extend(tags)
+
+    else:
+        latest_tags = parse_highest_verion(local_tags)
+        if latest_tags:
+            tags_to_push.append(f"v{latest_tags}")
+        else:
+            raise RuntimeError("Failed to push to remote, No local tag found.")
+
+    for tag in tags_to_push:
+        print(f"Pushing (v{tag}) to remote...")
+        push_to_remote(Version(tag))
